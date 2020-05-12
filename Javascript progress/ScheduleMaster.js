3743c557612efb320f70fdf22c14a149efd82d5d
jakabij <jakabistvanj@gmail.com>
2020-05-12 10:43:08 +0200

    Added another site. The UserControllPage.

--- Javascript progress/ScheduleMaster.js
+++ Javascript progress/ScheduleMaster.js
@@ -16,19 +16,36 @@ document.addEventListener("DOMContentLoaded",() => {
     grid.appendChild(element)
     
     element = document.createElement("a")
-    element.textContent = "ASD"
+    element.textContent = "Register"
     element.setAttribute("class","headerElement")
+    element.addEventListener("click",Register)
     grid.appendChild(element)
 })
 
+
 function Login (form) {
     let loginForm = document.querySelector("#loginForm")
-    loginForm.setAttribute("style","visibility: visible")
+    loginForm.setAttribute("style","display: unset")
 
     let username = form.username.value;
     let password = form.password.value;
     
     console.log("Name: "+username+" Password: "+password)
+    alert("Welcome!")
+    loginForm.setAttribute("style","display: none")
 
     //if logged in: document.querySelector("#login").textContent = "Logout"
+}
+
+
+function Register(form){
+    let registerForm = document.querySelector("#registerForm")
+    registerForm.setAttribute("style","display: unset")
+
+    let username = form.username.value;
+    let password = form.password.value;
+
+    console.log("Name: "+username+" Password: "+password)
+    alert("Welcome to our services noob!")
+    registerForm.setAttribute("style","display: none")
 }
