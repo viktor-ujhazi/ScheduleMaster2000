9b2e4e6dde0f72f2db20b9b7239d8c9fddc07ef4
jakabij <jakabistvanj@gmail.com>
2020-05-12 13:42:21 +0200

    Login/Register modified in main project (cshtml)

--- Javascript progress/ScheduleMaster.js
+++ Javascript progress/ScheduleMaster.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded",() => {
     element.textContent = "Login"
     element.setAttribute("class","headerElement")
     element.setAttribute("id","loginHeader")
-    element.addEventListener("click",Login)
+    element.addEventListener("click",LoginPage)
 
     grid.appendChild(element)
     
@@ -19,59 +19,84 @@ document.addEventListener("DOMContentLoaded",() => {
     element.textContent = "Register"
     element.setAttribute("class","headerElement")
     element.setAttribute("id","registerHeader")
-    element.addEventListener("click",Register)
+    element.addEventListener("click",RegisterPage)
     grid.appendChild(element)
 })
 
 
-function Login (form) {
-    let loginForm = document.querySelector("#loginForm")
-    loginForm.setAttribute("style","display: unset")
+function RegisterPage(){
+    let registerForm = document.querySelector("#registerForm");
+    registerForm.setAttribute("style","display: unset");
+};
 
+
+function LoginPage(){
+    let loginForm = document.querySelector("#loginForm");
+    loginForm.setAttribute("style","display: unset");
+};
+
+
+
+
+function Login (form) {
     let username = form.username.value;
     let password = form.password.value;
-    
-    console.log("Name: "+username+" Password: "+password)
-    alert("Welcome!")
-    loginForm.setAttribute("style","display: none")
+    console.log("Name: "+username+" Password: "+password);
+    alert("Welcome!");
+    loginForm.setAttribute("style","display: none");
 
-    let headerToHide = document.querySelector("#loginHeader")
-    headerToHide.setAttribute("style","display: none")
-    headerToHide = document.querySelector("#registerHeader")
-    headerToHide.setAttribute("style","display: none")
+    let headerToHide = document.querySelector("#loginHeader");
+    headerToHide.setAttribute("style","display: none");
+    headerToHide = document.querySelector("#registerHeader");
+    headerToHide.setAttribute("style","display: none");
 
 
-    element = document.createElement("a")
-    element.textContent = "Logout"
-    element.setAttribute("class","headerElement")
-    element.setAttribute("id","logoutHeader")
-    element.addEventListener("click",Logout)
-    grid.appendChild(element)
+    element = document.createElement("a");
+    element.textContent = "Logout";
+    element.setAttribute("class","headerElement");
+    element.setAttribute("id","logoutHeader");
+    element.addEventListener("click",Logout);
+    grid.appendChild(element);
 
-    //if logged in: document.querySelector("#login").textContent = "Logout"
+
+    element = document.createElement("a");
+    element.textContent = "Show Schedule";
+    element.setAttribute("class","headerElement");
+    element.setAttribute("id","scheduleHeader");
+    element.addEventListener("click",ShowSchedule);
+    grid.appendChild(element);
 }
 
 
 function Register(form){
-    let registerForm = document.querySelector("#registerForm")
-    registerForm.setAttribute("style","display: unset")
-
     let username = form.username.value;
     let password = form.password.value;
 
-    console.log("Name: "+username+" Password: "+password)
-    alert("Welcome to our services noob!")
-    registerForm.setAttribute("style","display: none")
+    console.log("Name: "+username+" Password: "+password);
+    alert("Welcome to our services noob!");
+    registerForm.setAttribute("style","display: none");
 }
 
 
 function Logout(){
-    let logoutHeader = document.querySelector("#logoutHeader")
-    grid.removeChild(logoutHeader)
+    let headerToRemove = document.querySelector("#logoutHeader")
+    grid.removeChild(headerToRemove)
 
-    let headerToHide = document.querySelector("#loginHeader")
-    headerToHide.setAttribute("style","display: unset")
+    headerToRemove = document.querySelector("#scheduleHeader")
+    grid.removeChild(headerToRemove)
 
-    headerToHide = document.querySelector("#registerHeader")
-    headerToHide.setAttribute("style","display: unset")
+    let headerToShow = document.querySelector("#loginHeader")
+    headerToShow.setAttribute("style","display: unset")
+
+    headerToShow = document.querySelector("#registerHeader")
+    headerToShow.setAttribute("style","display: unset")
+}
+
+
+function ShowSchedule(model){
+    model.array.forEach(function(row) {
+        console.log(row.title)
+        console.log(row.name)
+        console.log(row.adress)
+    });
 }
--- ScheduleMaster/wwwroot/js/site.js
+++ ScheduleMaster/wwwroot/js/site.js
@@ -1,77 +1,105 @@
-﻿const grid = document.querySelector("#headerGrid")
+﻿const grid = document.querySelector("#headerGrid");
 
 document.addEventListener("DOMContentLoaded", () => {
-    let element = document.createElement("a")
-    element.textContent = "Home"
-    element.setAttribute("id", "firstElement")
+    let element = document.createElement("a");
+    element.textContent = "Home";
+    element.setAttribute("id", "firstElement");
 
-    grid.appendChild(element)
+    grid.appendChild(element);
 
-    element = document.createElement("a")
-    element.textContent = "Login"
-    element.setAttribute("class", "headerElement")
-    element.setAttribute("id", "loginHeader")
-    element.addEventListener("click", Login)
+    element = document.createElement("a");
+    element.textContent = "Login";
+    element.setAttribute("class", "headerElement");
+    element.setAttribute("id", "loginHeader");
+    element.addEventListener("click", LoginPage);
 
-    grid.appendChild(element)
+    grid.appendChild(element);
 
-    element = document.createElement("a")
-    element.textContent = "Register"
-    element.setAttribute("class", "headerElement")
-    element.setAttribute("id", "registerHeader")
-    element.addEventListener("click", Register)
-    grid.appendChild(element)
-})
+    element = document.createElement("a");
+    element.textContent = "Register";
+    element.setAttribute("class", "headerElement");
+    element.setAttribute("id", "registerHeader");
+    element.addEventListener("click", RegisterPage);
+    grid.appendChild(element);
+});
+
+
+function RegisterPage() {
+    HideLoginPage()
+    let registerForm = document.querySelector("#registerForm");
+    registerForm.setAttribute("style", "display: unset");
+};
+
+
+function HideRegisterPage() {
+    let registerForm = document.querySelector("#registerForm");
+    registerForm.setAttribute("style", "display: none");
+}
+
+
+function LoginPage() {
+    HideRegisterPage()
+    let loginForm = document.querySelector("#loginForm");
+    loginForm.setAttribute("style", "display: unset");
+};
+
+
+function HideLoginPage() {
+    let loginForm = document.querySelector("#loginForm");
+    loginForm.setAttribute("style", "display: none");
+}
 
 
 function Login(form) {
-    let loginForm = document.querySelector("#loginForm")
-    loginForm.setAttribute("style", "display: unset")
-
     let username = form.username.value;
     let password = form.password.value;
+    console.log("Name: " + username + " Password: " + password);
+    alert("Welcome!");
+    loginForm.setAttribute("style", "display: none");
 
-    console.log("Name: " + username + " Password: " + password)
-    alert("Welcome!")
-    loginForm.setAttribute("style", "display: none")
-
-    let headerToHide = document.querySelector("#loginHeader")
-    headerToHide.setAttribute("style", "display: none")
-    headerToHide = document.querySelector("#registerHeader")
-    headerToHide.setAttribute("style", "display: none")
+    let headerToHide = document.querySelector("#loginHeader");
+    headerToHide.setAttribute("style", "display: none");
+    headerToHide = document.querySelector("#registerHeader");
+    headerToHide.setAttribute("style", "display: none");
 
 
-    element = document.createElement("a")
-    element.textContent = "Logout"
-    element.setAttribute("class", "headerElement")
-    element.setAttribute("id", "logoutHeader")
-    element.addEventListener("click", Logout)
-    grid.appendChild(element)
+    element = document.createElement("a");
+    element.textContent = "Logout";
+    element.setAttribute("class", "headerElement");
+    element.setAttribute("id", "logoutHeader");
+    element.addEventListener("click", Logout);
+    grid.appendChild(element);
 
-    //if logged in: document.querySelector("#login").textContent = "Logout"
+
+    //element = document.createElement("a");
+    //element.textContent = "Show Schedule";
+    //element.setAttribute("class", "headerElement");
+    //element.setAttribute("id", "scheduleHeader");
+    //element.addEventListener("click", ShowSchedule);
+    //grid.appendChild(element);
 }
 
 
 function Register(form) {
-    let registerForm = document.querySelector("#registerForm")
-    registerForm.setAttribute("style", "display: unset")
-
     let username = form.username.value;
     let password = form.password.value;
 
-    console.log("Name: " + username + " Password: " + password)
-    alert("Welcome to our services noob!")
-    registerForm.setAttribute("style", "display: none")
+    console.log("Name: " + username + " Password: " + password);
+    alert("Welcome to our services noob!");
+    registerForm.setAttribute("style", "display: none");
 }
 
 
 function Logout() {
-    let logoutHeader = document.querySelector("#logoutHeader")
-    grid.removeChild(logoutHeader)
+    let headerToRemove = document.querySelector("#logoutHeader");
+    grid.removeChild(headerToRemove);
 
-    let headerToHide = document.querySelector("#loginHeader")
-    headerToHide.setAttribute("style", "display: unset")
+    //headerToRemove = document.querySelector("#scheduleHeader");
+    //grid.removeChild(headerToRemove);
 
-    headerToHide = document.querySelector("#registerHeader")
-    headerToHide.setAttribute("style", "display: unset")
-}
+    let headerToShow = document.querySelector("#loginHeader");
+    headerToShow.setAttribute("style", "display: unset");
+
+    headerToShow = document.querySelector("#registerHeader");
+    headerToShow.setAttribute("style", "display: unset");
+};
